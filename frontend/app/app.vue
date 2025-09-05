e79b68fce0c4c65d995bb122791f7516d20182fd
chiffon1204 <github@chiffon-lab.coffee>
2025-09-05 01:33:58 +0000

    ちんちん

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -2,5 +2,10 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <h1>ちんぽにゃ！！！！！</h1>
+    <h2>tinitn</h2>
+    <NuxtWelcome />
+    <NuxtWelcome />
+    <NuxtWelcome />
   </div>
 </template>
