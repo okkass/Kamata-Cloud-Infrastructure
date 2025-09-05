593bedb46e41688b31eae1488655b3cf9e4e4a7b
ok0693 <piyo2003918@gmail.com>
2025-09-05 01:37:24 +0000

    poka

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Private Cloud </h1>
+    <h2>Welcome to Your Nuxt 3 App</h2>
+    <h3>Start by editing <code>app.vue</code></h3>
   </div>
 </template>
