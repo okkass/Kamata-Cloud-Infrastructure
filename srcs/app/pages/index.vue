d52190d42d5a06ed745c2149a48f3598f437384e
ok0693 <piyo2003918@gmail.com>
2025-09-17 00:22:48 +0000

    test

--- srcs/app/pages/index.vue
+++ srcs/app/pages/index.vue
@@ -2,9 +2,14 @@
   <div class="p-8 bg-gray-100 min-h-screen">
     <h1 class="text-3xl font-bold underline text-blue-600">
       Hello, Tailwind CSS!
+      {{ message }}
     </h1>
     <p class="mt-4 text-gray-700">
       NuxtプロジェクトへのTailwind CSSの導入が完了しました。
     </p>
   </div>
 </template>
+
+<script setup>
+const message = 'Welcome to your Nuxt.js project with Tailwind CSSSSSS!'
+</script>
