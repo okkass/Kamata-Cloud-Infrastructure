3eba948a5d585c73dbe4e8ebe71c99210c040b87
k022c0054 <k022c0054@g.neec.ac.jp>
2025-10-08 01:49:39 +0000

    GBで適切なサイズになるように

--- srcs/server/api/backup/index.get.ts
+++ srcs/server/api/backup/index.get.ts
@@ -5,12 +5,12 @@ export default defineEventHandler(() => {
       name: "backup-web01",
       description: "説明",
       createdAt: new Date().toISOString(),
-      size: 1919, // このsizeはバックアップファイル自体のサイズなので、そのままでOK
+      size: 32 * 1024 * 1024 * 1024, // 32GBをバイトで表現
       targetVirtualStorage: {
         id: "0ccc59cf-afa7-41fe-9b75-50372c8b7e09",
         name: "storage-1",
         //バイト単位の大きな数値に変更
-        size: 30 * 1024 * 1024 * 1024, // 30MBをバイトで表現
+        size: 30 * 1024 * 1024 * 1024, // 30GBをバイトで表現
         pool: "c8984344-4ffe-4dd5-bc99-c02fb2302224",
       },
     },
@@ -24,7 +24,7 @@ export default defineEventHandler(() => {
         id: "00b722d5-c675-4c17-a326-87ed84506d2d",
         name: "storage-1",
         //ここを修正: バイト単位の大きな数値に変更
-        size: 80 * 1024 * 1024 * 1024, // 80MBをバイトで表現
+        size: 80 * 1024 * 1024 * 1024, // 80GBをバイトで表現
         pool: "6bf9effd-6843-4eb6-b91b-aa7498250079",
       },
     },
