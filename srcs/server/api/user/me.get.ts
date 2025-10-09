8c02baceb1df2c605d5cc55ff094951d58a9f1b6
bukidai <admin@bukidai.com>
2025-10-09 06:57:41 +0000

    toriaezu

--- srcs/server/api/user/me.get.ts
+++ srcs/server/api/user/me.get.ts
@@ -2,18 +2,6 @@ import { verifyToken, KCIJWTPayload } from "../../utils/jwt";
 import { randomUUID } from "crypto";
 
 export default defineEventHandler(async (event) => {
-  const accessToken = getCookie(event, "access_token");
-  if (!accessToken) {
-    event.res.statusCode = 401;
-    return { message: "Unauthorized" };
-  }
-
-  const payload = await verifyToken(accessToken);
-  if (!payload) {
-    event.res.statusCode = 401;
-    return { message: "Invalid or expired token" };
-  }
-
   const user1 = {
     id: randomUUID(),
     name: "Alice",
