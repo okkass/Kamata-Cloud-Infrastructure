fe4a58094a430e92894f526e9f345125363c247c
ok0693 <piyo2003918@gmail.com>
2025-10-13 17:36:27 +0000

    feature(header): ヘッダーとサイドバーの変更、背景を白にした

---
+++ srcs/app/components/DefaultHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="bg-white text-sm text-gray-700 relative z-50 shadow-md px-4">
+    <div class="flex items-center justify-between h-16">
+      <div class="flex items-center">
+        <nuxt-link to="/" class="text-lg font-bold">KCI</nuxt-link>
+      </div>
+
+      <nav>
+        <ul class="flex space-x-4">
+          <li>
+            <nuxt-link to="/settings" class="hover:underline">Settings</nuxt-link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
--- srcs/app/layouts/default.vue
+++ srcs/app/layouts/default.vue
@@ -1,43 +1,22 @@
 <template>
-  <div class="relative flex min-h-screen bg-slate-100">
-    <UserSidebar :isAdmin="isAdmin" />
-    <main
-      class="flex-1 p-8 transition-all duration-300"
-      :class="{ 'ml-64': isSidebarOpen }"
-    >
-      <button
-        v-if="!isSidebarOpen"
-        @click="open"
-        class="fixed top-4 left-4 z-50 p-2 bg-slate-800 text-white rounded-md hover:bg-slate-700"
-      >
-        <IconMenu />
-      </button>
-      <slot />
-    </main>
+  <div class="relative min-h-screen bg-white">
+    <DefaultHeader />
+    <div class="flex">
+      <UserSidebar :isAdmin="isAdmin" />
+      <main class="flex-1 p-8 transition-all duration-300">
+        <slot />
+      </main>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-
-import { useSidebar } from "~/composables/useSidebar";
-
+import { useUser } from "~/composables/useUser";
 import UserSidebar from "~/components/Sidebar.vue";
+import DefaultHeader from "~/components/DefaultHeader.vue";
+const { isAdmin, fetchUser } = useUser();
 
-// Composableから状態と関数を取得
-
-const { isSidebarOpen, open } = useSidebar();
-
-// ユーザー情報を取得してisAdminを設定
-
-const { data } = await useFetch("/api/user/me", { method: "GET" });
-
-// レスポンスがエラーの場合はfalse
-const isAdmin = ref(
-  data.value &&
-    "isAdmin" in data.value &&
-    typeof (data.value as any).isAdmin === "boolean"
-    ? (data.value as any).isAdmin
-    : false
-);
+onMounted(() => {
+  fetchUser();
+});
 </script>
