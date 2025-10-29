f21a6f2a2373211c158cc9d8ecacc5698f449f39
k022c0054 <k022c0054@g.neec.ac.jp>
2025-10-29 05:31:37 +0000

    ネットワーク、ローカルストレージをモックが毛頭するように

--- srcs/server/api/storage-pools/local/[id].get.ts
+++ srcs/server/api/storage-pools/local/[id].get.ts
@@ -5,19 +5,21 @@ export default defineEventHandler(async (event) => {
   if (!validate(id)) {
     createError({ statusCode: 400, message: "Invalid UUID format" });
   }
-  let mock: Array<StoragePoolDTO> = [
+  let mock: Array<LocalStoragePoolDTO> = [
     {
       id: "532ca711-42e0-449c-bd1c-51af16f333b6",
       name: "node1-SSD",
-      type: "local",
       createdAt: new Date().toISOString(),
       totalSize: 512 * 1024 * 1_024 * 1_024,
       usedSize: 200 * 1024 * 1_024 * 1_024,
+      nodeId: "c53bf525-96b2-47f9-940e-6a84001afb61",
+      devFile: "/dev/sda1",
     },
     {
       id: "93061a02-e35d-4055-8bcd-a0002440fb89",
       name: "node1-nfs",
-      type: "network",
+      nodeId: "c53bf525-96b2-47f9-940e-6a84001afb61",
+      devFile: "/dev/sda1",
       createdAt: new Date().toISOString(),
       totalSize: 512 * 1024 * 1_024 * 1_024,
       usedSize: 200 * 1024 * 1_024 * 1_024,
