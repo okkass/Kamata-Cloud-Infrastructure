f9a408567119c17059ddf39d3b78a1646da26319
bukidai <admin@bukidai.com>
2025-11-12 01:51:16 +0000

    storage-poolsを復活。types、モックも対応

--- srcs/shared/types/storage-pools.ts
+++ srcs/shared/types/storage-pools.ts
@@ -1,7 +1,7 @@
 /**
- * ネットワークストレージプールオブジェクト
+ * ストレージプールオブジェクト
  */
-export interface NetworkStoragePoolDTO {
+export interface StoragePoolDTO {
   /**
    * ストレージプールを識別するための一意なID
    */
@@ -11,9 +11,13 @@ export interface NetworkStoragePoolDTO {
    */
   name: string;
   /**
+   * ストレージプールが属する物理ノードのID
+   */
+  nodeId: string;
+  /**
    * ストレージプールが作成された日時
    */
-  createdAt: string;
+  createdAt?: string;
   /**
    * ストレージプールの総サイズ（バイト単位）
    */
@@ -23,102 +27,7 @@ export interface NetworkStoragePoolDTO {
    */
   usedSize: number;
   /**
-   * 対応するローカルストレージプールのID
+   * ストレージプールがネットワークアクセス可能かどうかを示すフラグ
    */
-  localStoragePoolId: string;
-}
-/**
- * ローカルストレージプールオブジェクト
- */
-export interface LocalStoragePoolDTO {
-  /**
-   * ストレージプールを識別するための一意なID
-   */
-  id: string;
-  /**
-   * ストレージプールの名前
-   */
-  name: string;
-  /**
-   * ストレージプールが作成された日時
-   */
-  createdAt: string;
-  /**
-   * ストレージプールの総サイズ（バイト単位）
-   */
-  totalSize: number;
-  /**
-   * 使用中のストレージサイズ（バイト単位）
-   */
-  usedSize: number;
-  /**
-   * ストレージプールが存在する物理ノードのID
-   */
-  nodeId: string;
-  /**
-   * ストレージデバイスのパス
-   */
-  devFile: string;
-}
-
-/**
- * ローカルストレージプール作成リクエストオブジェクト
- */
-export interface LocalStoragePoolCreateRequestDTO {
-  /**
-   * ストレージプールの名前
-   */
-  name: string;
-  /**
-   * ストレージプールを作成する物理ノードのID
-   */
-  nodeId: string;
-  /**
-   * ストレージプールのサイズ（バイト単位）
-   */
-  size: number;
-  /**
-   * ストレージデバイスのパス
-   */
-  devFile: string;
-}
-
-/**
- * ストレージデバイスファイルオブジェクト
- */
-export interface DeviceFileDTO {
-  /**
-   * ストレージデバイスのパス
-   */
-  file: string;
-  /**
-   * ストレージデバイスのサイズ（バイト単位）
-   */
-  size: number;
-  /**
-   * ストレージデバイスのベンダー情報
-   */
-  vendor: string;
-  /**
-   * ストレージデバイスのモデル情報
-   */
-  model: string;
-}
-
-/**
- * ネットワークストレージプール作成リクエストオブジェクト
- */
-export interface NetworkStoragePoolCreateRequestDTO {
-  /**
-   * ストレージプールの名前
-   */
-  name: string;
-  /**
-   * ストレージプールのサイズ（バイト単位）
-   */
-  size: number;
-  /**
-   * 使用するローカルストレージのID
-   */
-  localStorageId: string;
+  hasNetworkAccess: boolean;
 }
