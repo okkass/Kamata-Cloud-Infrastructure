ec9d2c6a702ec325b17d98e735d4f2a8e9368414
ok0693 <k022c0098@g.neec.ac.jp>
2025-11-26 01:12:10 +0000

    interface kaeta!!

--- srcs/shared/types/dto/image/ImageCreateRequest.ts
+++ srcs/shared/types/dto/image/ImageCreateRequest.ts
@@ -1,9 +1,18 @@
 // ImageCreateRequest.ts
 // openapi ImageCreateRequest.yml から自動生成
-import type { ImageServerBase } from "./ImageServerBase";
+import type { ImageClientUpdatable } from "./ImageClientUpdatable";
 
 /**
  * 仮想マシンイメージ作成リクエストオブジェクトのインターフェース
  * ImageServerBaseを継承
  */
-export interface ImageCreateRequest extends ImageServerBase {}
+export interface ImageCreateRequest extends ImageClientUpdatable {
+    /** 仮想マシンイメージのname */
+    name: string;
+    /** 仮想マシンイメージの説明 */
+    description?: string;
+    /** ノードID */
+    nodeId: string;
+    /** imgファイル */
+    file: string;
+}
