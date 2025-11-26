ad9b087adb100e2ecf8ec24251b6021d658cf012
ok0693 <115209639+ok0693@users.noreply.github.com>
2025-11-27 08:50:36 +0900

    useInstanceTypeAddForm.ts の更新

--- srcs/app/composables/modal/useInstanceTypeAddForm.ts
+++ srcs/app/composables/modal/useInstanceTypeAddForm.ts
@@ -21,13 +21,13 @@ const validationSchema = toTypedSchema(
     name: z.string().min(1, "インスタンスタイプ名は必須です。"),
     cpuCores: z
       .number({
-        message: "CPUコア数は必須です。",
+        message: "数値を入力してください。",
       })
       .int("整数で入力してください。")
       .min(1, "1以上の値を入力してください。"),
     memorySizeInMb: z
       .number({
-        message: "メモリサイズは必須です。",
+        message: "数値を入力してください。",
       })
       .int("整数で入力してください。")
       .min(1, "1MB以上の値を入力してください。"),
