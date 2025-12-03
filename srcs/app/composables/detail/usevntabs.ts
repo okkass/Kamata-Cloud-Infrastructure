4d51a018b08859ab3424d98a0086b8f1f1d06c14
k_asano <k022c0084@g.neec.ac.jp>
2025-12-03 07:28:21 +0000

    8割完成

---
+++ srcs/app/composables/detail/usevntabs.ts
@@ -0,0 +1,23 @@
+// composables/detail/usevntabs.ts
+
+import type { Component } from "vue";
+
+export type VnTabConfig = {
+  label: string;
+  value: string;
+  loader?: () => Promise<any>;
+  component?: Component;
+};
+
+export const vnTabs: VnTabConfig[] = [
+  {
+    label: "基本情報",
+    value: "basic",
+    loader: () => import("~/components/detail/panels/VnTabBasic.vue"),
+  },
+  {
+    label: "サブネット",
+    value: "subnets",
+    loader: () => import("~/components/detail/panels/VnTabSubnets.vue"),
+  },
+];
