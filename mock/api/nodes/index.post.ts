a279977d5ccc36b851f18139f8ece01bb14dcc93
bukidai <admin@bukidai.com>
2025-12-09 06:09:41 +0000

    WIPです

--- mock/api/nodes/index.post.ts
+++ mock/api/nodes/index.post.ts
@@ -1,3 +1,7 @@
+import { z } from "zod";
+import type { NodeCreateRequest } from "@app/shared/types";
+import { addNode } from "../../services/nodesService";
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   console.log("Received body:", body);
