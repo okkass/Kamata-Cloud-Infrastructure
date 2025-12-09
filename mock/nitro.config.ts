2f8b7842045dcd8c9feb2a6b888f02697ec7558b
bukidai <admin@bukidai.com>
2025-12-09 05:05:37 +0000

    自前でヘッダー用意してるのでこっちはいらない

--- mock/nitro.config.ts
+++ mock/nitro.config.ts
@@ -3,7 +3,6 @@ import { resolve } from "node:path";
 export default defineNitroConfig({
   routeRules: {
     "/api/**": {
-      cors: true,
       headers: {
         "Access-Control-Allow-Origin": "http://localhost:3000",
         "Access-Control-Allow-Methods":
