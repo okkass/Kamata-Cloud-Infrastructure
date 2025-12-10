3e29cd09f133760c0f8dcfbbacd7df13a1cfec92
k022c0054 <k022c0054@g.neec.ac.jp>
2025-12-10 00:34:16 +0000

    DTOなる過去の遺物を削除

--- srcs/shared/types/dto/security-group/index.ts
+++
@@ -1,6 +0,0 @@
-export type { SecurityRuleCreateRequestDTO } from './SecurityRuleCreateRequestDTO';
-export type { SecurityGroupUpdateRequestDTO } from './SecurityGroupUpdateRequestDTO';
-export type { SecurityGroupCreateRequestDTO } from './SecurityGroupCreateRequestDTO';
-export type { SecurityRuleDTO } from './SecurityRuleDTO';
-export type { SecurityRuleUpdateRequestDTO } from './SecurityRuleUpdateRequestDTO';
-export type { SecurityGroupDTO } from './SecurityGroupDTO';
