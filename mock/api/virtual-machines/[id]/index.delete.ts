a8434f47a14764b577f043f6a1728cbeaa7087a2
k022c0054 <k022c0054@g.neec.ac.jp>
2025-12-10 07:43:37 +0000

    501を返します

--- mock/api/virtual-machines/[id]/index.delete.ts
+++ mock/api/virtual-machines/[id]/index.delete.ts
@@ -1,4 +1,8 @@
 export default defineEventHandler(async (event) => {
-  event.node.res.statusCode = 204;
-  return;
+  event.node.res.statusCode = 501;
+  return {
+    type: "Not Implemented",
+    detail: "This endpoint is not yet implemented",
+    status: 501,
+  };
 });
