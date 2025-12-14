04d079cfaece91116bb7ac1f0ca1a12cdfc2ec0a
k022c0054 <k022c0054@g.neec.ac.jp>
2025-12-14 01:50:37 +0000

    完全勝利

--- mock/nitro.config.ts
+++ mock/nitro.config.ts
@@ -1,7 +1,12 @@
 import { defineNitroConfig } from "nitropack/config";
+import { resolve } from "node:path";
 
 // https://nitro.build/config
 export default defineNitroConfig({
   compatibilityDate: "latest",
   srcDir: "server",
+  alias: {
+    "@app": resolve(__dirname, "../srcs"),
+    "@": resolve(__dirname, "./server"),
+  },
 });
