a69e11f054b30a37f6c13e9970277b21be980a26
bukidai <admin@bukidai.com>
2025-12-16 06:30:16 +0000

    ごめん

--- srcs/server/middleware/return-500.ts
+++ srcs/server/middleware/return-500.ts
@@ -1,4 +1,7 @@
 export default defineEventHandler((event) => {
+  if (event.path.startsWith("/api/_nuxt_icon/")) {
+    return;
+  }
   if (event.path.startsWith("/api/")) {
     setResponseStatus(event, 501);
     return { message: "Not Implemented" };
