0c128be5763bd482787faa11092e31f07ef67e10
k022c1018 <114965614+k024c6001@users.noreply.github.com>
2025-12-24 05:59:27 +0000

    リクエスト送信可能に

--- srcs/app/composables/modal/useInstanceTypeEditForm.ts
+++ srcs/app/composables/modal/useInstanceTypeEditForm.ts
@@ -10,11 +10,13 @@ import {
   type InstanceTypeUpdateInput,
 } from "~/utils/validations/instance-type";
 import { useFormAction } from "./useModalAction";
+import { convertUnitToByte } from "~/utils/format";
 
 type Props = ModalFormProps<InstanceTypeResponse>;
 
 export const useInstanceTypeEditForm = (props: Props) => {
   const { handleFormSubmit, makeHandleClose } = useFormAction();
+  const { addToast } = useToast();
 
   // [推奨]: useResourceUpdater の戻り値を活用
   const {
@@ -24,6 +26,7 @@ export const useInstanceTypeEditForm = (props: Props) => {
     init,
     save: updaterSave,
     errorMessage,
+    isDirty,
   } = useResourceUpdater<InstanceTypeResponse>();
 
   // エラーメッセージを同期
@@ -74,12 +77,37 @@ export const useInstanceTypeEditForm = (props: Props) => {
     { immediate: true }
   );
 
+  // --- vee-validate の値を editedData に同期 ---
+  watch(
+    () => ({
+      name: name.value,
+      cpuCore: cpuCore.value,
+      memorySize: memorySizeField.value,
+    }),
+    (newValues) => {
+      if (editedData.value) {
+        editedData.value.name = newValues.name;
+        editedData.value.cpuCore = newValues.cpuCore;
+        if (newValues.memorySize != null) {
+          editedData.value.memorySize = convertUnitToByte(
+            newValues.memorySize,
+            "MB"
+          );
+        }
+      }
+    },
+    { deep: true }
+  );
+
   /**
    * 保存処理
    * [推奨]: useResourceUpdater の save() を使用して標準化
    */
-  const save = (emit: any) =>
-    handleFormSubmit<InstanceTypeUpdateInput, InstanceTypeResponse>(
+  const save = (emit: any) => {
+    const submitHandler = handleFormSubmit<
+      InstanceTypeUpdateInput,
+      InstanceTypeResponse
+    >(
       handleSubmit,
       {
         execute: async () => {
@@ -94,6 +122,18 @@ export const useInstanceTypeEditForm = (props: Props) => {
       },
       emit
     );
+
+    return async (e?: Event) => {
+      if (!isDirty.value) {
+        addToast({
+          message: "変更がありません。",
+          type: "info",
+        });
+        return;
+      }
+      return submitHandler(e);
+    };
+  };
 
   const close = (emit: any) => makeHandleClose(resetForm, emit);
 
