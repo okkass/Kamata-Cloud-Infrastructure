6e08f05202687f422af9a4831f0447add7833dc6
k022c0054 <k022c0054@g.neec.ac.jp>
2026-01-01 06:26:08 +0000

    認証関連APIを整備

--- backend/server/service/AuthService.ts
+++ backend/server/service/AuthService.ts
@@ -1,1 +1,11 @@
 import type { LoginRequest, LoginResponse } from "@app/shared/types";
+import UserRepository from "@/repository/UserRepository";
+import * as argon2 from "argon2";
+import type { ServiceError } from "@/common/errors";
+import type { Result } from "@/common/type";
+
+type AuthService = {
+  login: (req: LoginRequest) => Promise<Result<LoginResponse, ServiceError>>;
+  logout: (token: string) => Promise<Result<null, ServiceError>>;
+  refresh: (token: string) => Promise<Result<LoginResponse, ServiceError>>;
+};
