c5aec20aa876cbab5c76e257d8c641d3b2c79ed3
bukidai <admin@bukidai.com>
2026-01-16 06:05:29 +0000

    パワー

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -3,11 +3,23 @@ set -e
 
 # setup pnpm
 pnpm install --frozen-lockfile
+
 # set prisma
 cd backend
+# 一旦tsconfigを退避
+mv tsconfig.json tsconfig.tmp.json
+cp ../.devcontainer/backend/tsconfig.json.temp tsconfig.json
+# prismaのセットアップ
 pnpx prisma migrate dev
-pnpx prisma db seed
 pnpx prisma generate
+pnpx prisma db seed
+
+# 退避しておいたtsconfigを戻す
+rm tsconfig.json
+mv tsconfig.tmp.json tsconfig.json
+
+# nitroをビルド
+pnpm run build
 
 # setup .venv
 cd ../tests
